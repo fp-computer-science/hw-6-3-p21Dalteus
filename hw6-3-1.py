6c2e1021e12091c972545793424b46eac7893824
Darick <p21dalteus@fairfieldprep.org>
2020-10-29 23:13:04 -0400

    hw

---
+++ hw6-3-1.py
@@ -0,0 +1,35 @@
+# Author DA (AMDG) 10/27/2020
+
+import math
+
+# 1
+print(math.sqrt(529))
+
+# 2
+print(math.sin(0))
+
+# 3
+pi = math.pi
+print(math.cos(2 * pi))
+
+# 4
+print(math.pow(2.5, 5))
+
+# 5
+e = 2.71828
+print(math.log10(e))
+
+# 6
+print(math.pow(e, 4))
+
+# 7
+print(math.ceil(-12.9))
+
+# 8
+print(math.floor(7.25))
+
+# 9
+print(math.log(2 * 1000))
+
+# 10
+print(math.radians(180)
